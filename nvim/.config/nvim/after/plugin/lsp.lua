4ec448952129c1ad81f2b4771139d8a9b97b3e53
tonocarvacho <tonocarvacho@gmail.com>
2023-11-19 06:03:00 -0300

    fix: tmuxer location

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -17,7 +17,7 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'tsserver', 'rust_analyzer', 'jdtls'},
+  ensure_installed = {'tsserver', 'rust_analyzer', 'jdtls', 'lua-language-server'},
   handlers = {
     lsp_zero.default_setup,
     lua_ls = function()
